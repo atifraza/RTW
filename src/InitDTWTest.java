ca275f73c3246297d98bf0dec0e08040a45b7220
Atif Raza <atifraza@uni-mainz.de>
2015-04-10 16:40:13 +0200

    Adds and Improves the Apache Commons CLI options Assigns the program options from the CLI arguments while providing default values for the execution of the program

--- src/InitDTWTest.java
+++ src/InitDTWTest.java
@@ -12,13 +12,27 @@ import org.apache.commons.cli.HelpFormatter;
 import utils.dtw.*;
 
 public class InitDTWTest {
-	protected static String fileSwitch = "file";
-	protected static String helpSwitch = "help";
-	protected static String windowSwitch = "window";
-	protected static String twTypeSwitch = "warping";
-	protected static String rngSwitch = "rng";
-	protected static String restartsSwitch = "restarts";
-	protected static String rankingTypeSwitch = "ranking";
+	protected static String fileSwitch = "f",
+							fileSwitchLong = "file";
+	
+	protected static String helpSwitch = "h",
+							helpSwitchLong = "help";
+	
+	protected static String bandSwitch = "b",
+							bandSwitchLong = "band";
+	
+	protected static String twTypeSwitch = "t",
+							twTypeSwitchLong = "type";
+	
+	protected static String rngSwitch = "g",
+							rngSwitchLong = "rng";
+	
+	protected static String passesSwitch = "p",
+							passesSwitchLong = "passes";
+	
+	protected static String rankingTypeSwitch = "r",
+							rankingTypeSwitchLong = "ranking";
+	
 	protected static String segmentSwitch = "segment";
 	
 	public static void main(String[] args) {
@@ -37,13 +51,13 @@ public class InitDTWTest {
 			}
 			
 			fileName = cmdLine.getOptionValue(fileSwitch);
-			window = Integer.parseInt(cmdLine.getOptionValue(windowSwitch, "100"));
+			window = Integer.parseInt(cmdLine.getOptionValue(bandSwitch, "100"));
 			dtwType = cmdLine.getOptionValue(twTypeSwitch, "N");
 			
 			if (dtwType.equals("H")) {
 				rng = cmdLine.getOptionValue(rngSwitch, "N");
 				ranking = cmdLine.getOptionValue(rankingTypeSwitch, "L");					
-				passes = cmdLine.getOptionValue(restartsSwitch, "5");
+				passes = cmdLine.getOptionValue(passesSwitch, "5");
 			}
 			
 			if (cmdLine.hasOption(segmentSwitch)) {
@@ -69,19 +83,9 @@ public class InitDTWTest {
 				dtw.execute();
 				break;
 			case "H":
-				dtw = new HeuristicDTW(fileName, window, ranking, passes, rng);
+				dtw = new HeuristicDTW(fileName, window, ranking, passes, rng, start, inc);
 				dtw.execute();
 				break;
-//			case "U":
-//				dtw = new HeuristicDTW(fileName, window, ranking, passes, 1);
-////				dtw = new HeuristicDTW(fileName, window, ranking, passes, 1, start, inc);
-//				dtw.execute();
-//				break;
-//			case "G":
-//				dtw = new HeuristicDTW(fileName, window, ranking, passes, 2);
-////				dtw = new HeuristicDTW(fileName, window, ranking, passes, 2, start, inc);
-//				dtw.execute();
-//				break;
 		}
 		System.out.println("Done");
 	}
@@ -108,35 +112,38 @@ public class InitDTWTest {
 	
     private static Options constructCLIOptions() {
     	Option help = new Option(helpSwitch, "prints the help for the program");
+    	help.setLongOpt(helpSwitchLong);
     	
     	Option fileName = new Option(fileSwitch, "name of the dataset");
+    	fileName.setLongOpt(fileSwitchLong);
     	fileName.setRequired(true);
     	fileName.setArgs(1);
     	fileName.setArgName("dataset");
     	
-    	Option winSz = new Option(windowSwitch, "DTW window size as a percentage e.g. 1, 5, 10 100 (default)");
-//    	winSz.setRequired(true);
+    	Option winSz = new Option(bandSwitch, "DTW window size as a percentage e.g. 1, 5, 10 100 (default)");
+    	winSz.setLongOpt(bandSwitchLong);
     	winSz.setArgs(1);
     	winSz.setArgName("percent");
-//    	winSz.setType(Number.class);
     	
     	Option dtwType = new Option(twTypeSwitch, "defines the DTW type, valid options are N (default), L, and H for Normal, Lucky, and Heuristic");
-//    	type.setRequired(true);
+    	dtwType.setLongOpt(twTypeSwitchLong);
     	dtwType.setArgs(1);
     	dtwType.setArgName("warpingType");
     	
     	Option rngType = new Option(rngSwitch, "defines the RNG type, valid options are N (normally distributed random numbers - default), "+
     								"U (uniformly distributed random numbers)");
-//    	type.setRequired(true);
+    	rngType.setLongOpt(rngSwitchLong);
     	rngType.setArgs(1);
     	rngType.setArgName("rngType");
     	
-    	Option passes = new Option(restartsSwitch, "number of restarts for heuristic DTW calculations 0 (no restarts), " + 
+    	Option passes = new Option(passesSwitch, "number of restarts for heuristic DTW calculations 0 (no restarts), " + 
     							   "I (increasing with number of runs), positive integer (for constant number of restarts)");
+    	passes.setLongOpt(passesSwitchLong);
     	passes.setArgs(1);
     	passes.setArgName("number");
     	
     	Option ranking = new Option(rankingTypeSwitch, "type of ranking to use e.g. L (linear - default), E (exponential)");
+    	ranking.setLongOpt(rankingTypeSwitchLong);
     	ranking.setArgs(1);
     	ranking.setArgName("method");
     	
